4940fe31042202e0dd1799322957ce4e2c572971
MeAmatuer <sandipan2602@gmail.com>
2019-05-08 02:20:50 +0530

    Implemented UserProfile Endpoint. Uncommented the CommonControllerTest Class.

--- quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
+++ quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
@@ -66,4 +66,14 @@ public class RestExceptionHandler {
                 .message(exc.getErrorMessage()),
                 HttpStatus.UNAUTHORIZED);
     }
+
+//    @ExceptionHandler(AuthorizationFailedException.class)
+//    public ResponseEntity<ErrorResponse> userAuthorizationFailedException(AuthorizationFailedException exc, WebRequest request){
+//        return new ResponseEntity<ErrorResponse>(new ErrorResponse()
+//                .code(exc.getCode())
+//                .message(exc.getErrorMessage()),
+//                HttpStatus.UNAUTHORIZED);
+//    }
+
+
 }
